eb2899e9a4892f51e89df2ec2a67f2d3122f0d3f
Sahat Yalkabov <sakhat@gmail.com>
2014-03-12 00:14:04 -0400

    File re-organization, switched Grunt for Gulp

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass')
+
+gulp.task('sass', function () {
+  gulp.src('./styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./styles'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./styles/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
