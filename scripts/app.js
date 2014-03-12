7c3c5b94b4a4a2edd7e009d874625fe5db39f9ce
Sahat Yalkabov <sakhat@gmail.com>
2014-03-12 03:10:39 -0400

    Added dynamic url: /backend/:language

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,23 @@
 'use strict';
 
-angular.module('MyApp', ['ngRoute'])
+angular.module('MyApp', ['ngRoute', 'ui.bootstrap'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/backend', {
+        templateUrl: 'views/backend.html',
+        controller: 'MainCtrl'
+      })
+      .when('/backend/:language', {
+        templateUrl: function(params) {
+          return 'views/' + params.language + '.html';
+        }
+      })
+      .when('/frontend', {
+        templateUrl: 'views/frontend.html',
         controller: 'MainCtrl'
       })
       .otherwise({
