dce4f5c1840eba55f9cb2e12ee2dfc53efd95e3f
Sahat Yalkabov <sakhat@gmail.com>
2014-03-19 03:25:05 -0400

    Refactored document json into a separate file

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,61 +1,24 @@
 'use strict';
 
 angular.module('MyApp')
-  .controller('MainCtrl', function($scope, $routeParams) {
+  .controller('MainCtrl', function($scope, $http, $routeParams) {
 
-    $scope.backend = {
-      beginner: [
-        {
-          title: 'Introduction to Jade',
-          slug: 'introduction-to-jade',
-          description: 'Brief crash course into Jade templating language'
-        },
-        {
-          title: 'Form submission in Express',
-          slug: 'form-submission-in-express',
-          description: 'Learn how to POST forms in Express'
-        },
-        {
-          title: 'File Upload',
-          slug: 'file-upload',
-          description: 'Uploading user files'
-        },
-        {
-          title: 'Useful Node.js utilities',
-          slug: 'useful-nodejs-utlities'
-        },
-        {
-          title: 'Socket.IO 101',
-          slug: 'socketio-101'
-        },
-        {
-          title: 'Creating a simple MongoDB Schema',
-          slug: 'creating-a-simple-mongodb-schema'
-        },
-        {
-          title: 'Logging',
-          slug: 'logging'
-        },
-        {
-          title: 'Node.js Debugging',
-          slug: 'nodejs-debugging'
-        }
-      ],
-      intermediate: [],
-      advanced: []
-    };
+    $http.get('docs.json')
+      .then(function(res) {
+        $scope.backend = res.data.backend;
 
-    var currentPage = _.findWhere($scope.backend.beginner, { slug: $routeParams.name });
+        var currentPage = _.findWhere($scope.backend.beginner, { slug: $routeParams.name });
 
-    $scope.getTitle = function() {
-      return currentPage.title;
-    };
+        $scope.getTitle = function() {
+          return currentPage.title;
+        };
 
-    $scope.getDescription = function() {
-      return currentPage.description;
-    };
+        $scope.getDescription = function() {
+          return currentPage.description;
+        };
 
-    $scope.docUrl = function() {
-      return 'docs/' + $routeParams.name + '.md';
-    };
+        $scope.docUrl = function() {
+          return 'docs/' + $routeParams.name + '.md';
+        };
+      });
   });
