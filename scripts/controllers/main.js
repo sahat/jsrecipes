9275fb7c07fca8c5978ca22dbb23ecce693daf80
Sahat Yalkabov <sakhat@gmail.com>
2014-03-20 01:13:05 -0400

    Updated and refactored main controlled based on Ange blogging engine

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,25 +1,19 @@
-'use strict';
-
 angular.module('MyApp')
-  .controller('MainCtrl', function($scope, $http, $routeParams) {
-
-    $http.get('docs.json')
-      .then(function(res) {
-        $scope.backend = res.data.backend;
-        $scope.frontend = res.data.frontend;
-
-        var currentPage = _.findWhere($scope.backend.beginner, { slug: $routeParams.name });
-
-        $scope.getTitle = function() {
-          return currentPage.title;
-        };
-
-        $scope.getDescription = function() {
-          return currentPage.description;
-        };
-
-        $scope.docUrl = function() {
-          return 'docs/' + $routeParams.name + '.md';
-        };
+  .controller('MainCtrl', function($scope, $window, Posts, $routeParams, ngProgress) {
+    $scope.$on('$routeChangeSuccess', function($currentRoute, $previousRoute) {
+        if ($routeParams.name) {
+          ngProgress.start();
+          Posts.getBySlug($routeParams.name, function(data) {
+            $scope.page = data;
+            ngProgress.complete();
+            $window.document.title = $scope.page.title + " - " + 'JS Recipes'
+          });
+        } else {
+          $window.document.title = 'JS Recipes' + " - " + 'Site Description';
+          Posts.getPosts(function(data) {
+            console.log(data)
+            $scope.docs = data;
+          });
+        }
       });
   });
