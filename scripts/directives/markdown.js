fc071b98da53eb7a5a06ba9858fc0e2cd59ac48b
Sahat Yalkabov <sakhat@gmail.com>
2014-03-20 02:02:37 -0400

    Modified markdown directive

--- scripts/directives/markdown.js
+++ scripts/directives/markdown.js
@@ -1,11 +1,17 @@
 angular.module('MyApp')
-  .directive('markdown', function() {
-    var converter = new Showdown.converter({ extensions: ['table', 'twitter', 'github'] });
+  .directive('markdown', function($http) {
+    var converter = new Showdown.converter();
     return {
       restrict: 'A',
       link: function(scope, element, attrs) {
-        var html = converter.makeHtml(element.text());
-        element.html(html);
+        attrs.$observe('link', function(link) {
+          if (link){
+            $http.get('posts/' + link).success(function(response) {
+              var htmlText = converter.makeHtml(response);
+              element.html(htmlText);
+            });
+          }
+        });
       }
     }
   });
