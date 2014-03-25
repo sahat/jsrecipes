38f64d6550a7c9fbf6252ed094781129e92e0050
Sahat Yalkabov <sakhat@gmail.com>
2014-03-25 17:50:59 -0400

    Removed restrict property; defaults to 'A'.

--- scripts/directives/markdown.js
+++ scripts/directives/markdown.js
@@ -2,14 +2,15 @@ angular.module('MyApp')
   .directive('markdown', function($http) {
     var converter = new Showdown.converter();
     return {
-      restrict: 'A',
       link: function(scope, element, attrs) {
         attrs.$observe('link', function(link) {
-          if (link){
+          if (link) {
             $http.get('posts/' + link).success(function(response) {
               var htmlText = converter.makeHtml(response);
               element.html(htmlText);
-              $('pre code').each(function(i, e) { hljs.highlightBlock(e)} );
+              $('pre code').each(function(i, e) {
+                hljs.highlightBlock(e)
+              });
             });
           }
         });
