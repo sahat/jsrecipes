9ee5d625ab7ff69c99f0bda389715aa2f72146f4
Sahat Yalkabov <sakhat@gmail.com>
2014-03-25 18:04:23 -0400

    $compile HTML to enable directives in markdown

--- scripts/directives/markdown.js
+++ scripts/directives/markdown.js
@@ -1,5 +1,5 @@
 angular.module('MyApp')
-  .directive('markdown', function($http) {
+  .directive('markdown', function($http, $compile) {
     var converter = new Showdown.converter();
     return {
       link: function(scope, element, attrs) {
@@ -8,8 +8,9 @@ angular.module('MyApp')
             $http.get('posts/' + file).success(function(response) {
               var htmlText = converter.makeHtml(response);
               element.html(htmlText);
+              $compile(element.contents())(scope);
               $('pre code').each(function(i, e) {
-                hljs.highlightBlock(e)
+                hljs.highlightBlock(e);
               });
             });
           }
