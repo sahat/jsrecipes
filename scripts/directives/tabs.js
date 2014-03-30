a8592ff154e28e60e15aafddb20c15dc62e35abd
Sahat Yalkabov <sakhat@gmail.com>
2014-03-30 15:36:03 -0400

    added tabs example, but will not be used yet until it is refactored to be more reusable

---
+++ scripts/directives/tabs.js
@@ -0,0 +1,32 @@
+/**
+ * TODO: Refactor. Make it re-usable accross markdown files.
+ */
+
+//<ul class="nav nav-tabs">
+//  <li ng-init="activeTab = 'signup'"
+//  ng-class="{'active': activeTab === 'signup'}"
+//  ng-click="activeTab = 'signup'">
+//    <a>Signup</a>
+//  </li>
+//  <li class="pure-button"
+//  ng-class="{'active': activeTab === 'login'}"
+//  ng-click="activeTab = 'login'">
+//    <a>Login</a>
+//  </li>
+//</ul>
+//<div class="tab-content">
+//  <div ng-switch="activeTab">
+//    <div ng-switch-when="signup">
+//    Example!
+//      <pre>
+//      mongoose.connect('localhost');
+//      mongoose.connection.on('error', function(err) {
+//        log.error(err);
+//        });
+//      </pre>
+//    </div>
+//    <div ng-switch-when="login">
+//    login
+//    </div>
+//  </div>
+//  </div>
