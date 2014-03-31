97461af7179869fa643e35809e9958a14afde1b0
Sahat Yalkabov <sakhat@gmail.com>
2014-03-31 02:48:37 -0400

    Created a custom service that fetches latest commit from github and uses the date to display in detail.html when document was last updated

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,12 +1,17 @@
 angular.module('MyApp')
-  .controller('MainCtrl', function($scope, $window, Posts, $routeParams, ngProgress) {
+  .controller('MainCtrl', function($scope, $window, Posts, GitHub, $routeParams, ngProgress) {
     $scope.$on('$routeChangeSuccess', function($currentRoute, $previousRoute) {
         if ($routeParams.name) {
           ngProgress.start();
-          Posts.getBySlug($routeParams.name, function(data) {
-            $scope.post = data;
+          Posts.getBySlug($routeParams.name, function(post) {
+            $scope.post = post;
             ngProgress.complete();
             $window.document.title = $scope.post.title + " - " + 'JS Recipes'
+
+            // Get last commit date
+            GitHub.lastCommit(post.file, function(data) {
+              $scope.lastUpdated = new Date(data[0].commit.committer.date).toLocaleString();
+            });
           });
         } else {
           $window.document.title = 'JS Recipes' + " - " + 'Site Description';
---
+++ scripts/services/github.js
@@ -0,0 +1,8 @@
+angular.module('MyApp')
+  .factory('GitHub', function($http) {
+    return {
+      lastCommit: function(file, callback) {
+        $http.get('https://api.github.com/repositories/17648824/commits?path=posts/' + file).success(callback);
+      }
+    }
+  });
