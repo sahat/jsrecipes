fa70e3fa3304e4729fb646bc6d4098b0f241f6f4
Sahat Yalkabov <sakhat@gmail.com>
2014-04-04 02:58:01 -0400

    Added gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
 gulp.task('sass', function () {
   gulp.src('./styles/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./styles'));
+});
+
+gulp.task('compress', function() {
+  gulp.src('lib/*.js')
+    .pipe(uglify({outSourceMap: true}))
+    .pipe(gulp.dest('dist'))
 });
 
 gulp.task('watch', function() {
